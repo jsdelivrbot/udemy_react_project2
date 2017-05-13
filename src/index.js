2b17014b4b4a0e6dceb569b032ead8a4c45aed5d
Chris <synthetic_samurai@hotmail.com>
2017-05-13 19:59:33 -0400

    uploaded new project

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'React-dom';
+
+const API_KEY = 'AIzaSyBFUSpk9wq5M799B7U8ZgR_L1oEfTRogXw';
+
+//create a new component. This component should produce some html
+const App = () => {
+	return <div>hi tomo</div>;
+}
+
+//take this component's generated html and put it on the page ( in the dom )
+ReactDOM.render(<App />, document.querySelector('.container'));
