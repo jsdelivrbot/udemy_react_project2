03de28ab05ad1606cf1797e14e4ce299e3736fda
Chris <synthetic_samurai@hotmail.com>
2017-05-13 20:01:23 -0400

    created new branch

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const API_KEY = 'AIzaSyBFUSpk9wq5M799B7U8ZgR_L1oEfTRogXw';
 
 //create a new component. This component should produce some html
 const App = () => {
-	return <div>hi tomo</div>;
+	return <div>hi</div>;
 }
 
 //take this component's generated html and put it on the page ( in the dom )
